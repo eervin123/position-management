8d13832cb49433a3bf19c2a829d7668484d28909
Eric Ervin <eervin@blockforcecapital.com>
2023-06-16 12:39:31 -0700

    added best_params for ppl to keep a log

---
+++ best_params.py
@@ -0,0 +1,26 @@
+best_params_2021 = {
+    "initial_position_size": 0.5,
+    "percent_invested_threshold": 0.4,
+    "atr_length": 14,
+    "atr_multiplier": 0.4,
+    "add_size": 0.1,
+    "delay_period": 250,
+    "delta_time": 800,
+    "upper_bound_profit_target": 0.03,
+    "lower_bound_loss_threshold": -0.039999999999999994,
+    "deleverage_pct": 0.5,
+    "max_loss_threshold": -0.15,
+}
+
+best_params_full_with_max_hold_length ={'initial_position_size': 0.4,
+ 'percent_invested_threshold': 0.5,
+ 'atr_length': 12,
+ 'atr_multiplier': 0.4,
+ 'add_size': 0.05,
+ 'delay_period': 250,
+ 'delta_time': 900,
+ 'upper_bound_profit_target': 0.02,
+ 'lower_bound_loss_threshold': -0.1,
+ 'deleverage_pct': 0.5,
+ 'max_loss_threshold': -0.09999999999999999,
+ 'max_hold_length': 1680}
